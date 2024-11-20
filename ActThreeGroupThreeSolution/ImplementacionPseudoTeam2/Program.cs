92699a413b36809c285593e4ad4893e1335871a7
Sebastian Zadomen <sebastian.zadomen.7e8@itb.cat>
2024-11-20 11:56:52 +0100

    Refactor input name

--- ActThreeGroupThreeSolution/ImplementacionPseudoTeam2/Program.cs
+++ ActThreeGroupThreeSolution/ImplementacionPseudoTeam2/Program.cs
@@ -5,6 +5,7 @@ namespace MyProgram
     {
         // const
         const string MsgErrorDades = "Les dades han sigut introduïdes incorrectament";
+        const string MsgErrorDadesEsp = "Les dades contenten simbols no permesos";
         const string MsgUserAvatar = "Tria el avatar!Has d'introduir un nombre entre 1 i 4.";
         const string MsgUserName = "Tria un nom, sense números, que com a mínim tingui 2 lletres i com màxim 25.";
         const string MsgMaldat = "Tria el teu nivell de maldat!Has d'introduir un nombre entre 1000 i 50000.";
@@ -26,7 +27,7 @@ namespace MyProgram
 
             // main
             AvatarSelector(ref userAvatar);
-            NameSelector(ref userName, userNameArray);
+            userName = InputName();
             MaldatSelector(ref userMaldat);
             GameCore(userNameArray, vocalesArray,ref numVocales,ref polsMagica, userMaldat);  // se pasan los parámetros
            
@@ -52,27 +53,88 @@ namespace MyProgram
 
             while (int.TryParse(Console.ReadLine(), out userNumber) && ValidNumber(MaxAvatar, MinAvatar, userNumber));
         }
-        public static void NameSelector(ref string userName, string userNameArray)
+        public static string InputName()
         {
+            string userName = "";
             //Les variables estaven declarades al demanar la funció en comptes d'en la pròpia funció.
             Console.WriteLine(MsgUserName);
             do
             {
                 userName = Console.ReadLine();
-                if (!(userName.Length >= 2 && userName.Length <= 25))
+                if (!(CheckUserNameLenght(userName)))
                 {
-                    Console.WriteLine(MsgErrorDades);
+                    //throw exception en comptes de writeline amb l'error
+                    throw new ArgumentOutOfRangeException(MsgErrorDades);
                 }
-            } while (!(userName.Length >= 2 && userName.Length <= 25));
-            //La condició estava invertida
-            userNameArray = userName;
-            //havien igualat al revès
+
+                else if (CheckSpecialChar(userName))
+                {
+                    throw new FormatException(MsgErrorDadesEsp);
+                }
+            }
+            //La condició estava invertida i no comprovava caràcters especials
+            while (!(CheckUserNameLenght(userName) && CheckSpecialChar(userName)));
+
+            return userName;
+
         }
+
+        static public bool CheckSpecialChar(string userName)
+        {
+            userName = userName.ToLower();
+            for (int i = 0; i < userName.Length; i++)
+            {
+                if (!(userName[i] == 'a' ||
+                    userName[i] == 'b' ||
+                    userName[i] == 'c' ||
+                    userName[i] == 'd' ||
+                    userName[i] == 'e' ||
+                    userName[i] == 'f' ||
+                    userName[i] == 'g' ||
+                    userName[i] == 'h' ||
+                    userName[i] == 'i' ||
+                    userName[i] == 'j' ||
+                    userName[i] == 'k' ||
+                    userName[i] == 'l' ||
+                    userName[i] == 'm' ||
+                    userName[i] == 'n' ||
+                    userName[i] == 'o' ||
+                    userName[i] == 'p' ||
+                    userName[i] == 'q' ||
+                    userName[i] == 'r' ||
+                    userName[i] == 's' ||
+                    userName[i] == 't' ||
+                    userName[i] == 'u' ||
+                    userName[i] == 'v' ||
+                    userName[i] == 'w' ||
+                    userName[i] == 'x' ||
+                    userName[i] == 'y' ||
+                    userName[i] == 'z' ||
+                    userName[i] == '0' ||
+                    userName[i] == '1' ||
+                    userName[i] == '2' ||
+                    userName[i] == '3' ||
+                    userName[i] == '4' ||
+                    userName[i] == '5' ||
+                    userName[i] == '6' ||
+                    userName[i] == '7' ||
+                    userName[i] == '8' ||
+                    userName[i] == '9')) ;
+            }
+            return false;
+        }
+
+        private static bool CheckUserNameLenght(string userName)
+        {
+            return userName.Length >= 2 && userName.Length <= 25;
+        }
+        
         public static void GameCore(string userNameArray, char[] vocalesArray, ref int numVocales, ref int polsMagica, int userMaldat)   // se pasan los parámetros
         {
             CountVowels(userNameArray, vocalesArray, ref numVocales);
             DistributeDust(ref polsMagica, numVocales, userMaldat);
         }
+        
         public static void CountVowels(string userNameArray, char[] vocalesArray, ref int numVocales)
         {
             for (int i = 0; i < userNameArray.Length; i++)
